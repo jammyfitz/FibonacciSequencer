104684d661fff9c51f745bc426390c03436e6ffb
Jamie Fitzgerald <Jamie.Fitzgerald@hotels4u.com>
2016-05-24 15:58:34 +0100

    Initial Commit

---
+++ FibonacciSequence/Sequencer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace FibonacciSequence
+{
+    public class Sequencer
+    {
+        private int _currentNumber;
+        private int _mostPreviousNumber;
+        private int _immediatelyPreviousNumber;
+        private List<int> _fibonacciSequence;
+
+        public Sequencer()
+        {
+            _currentNumber = 1;
+            _immediatelyPreviousNumber = 1;
+            _mostPreviousNumber = 0;
+            _fibonacciSequence = Generate(100);
+        }
+
+        public int GetPosition(int position)
+        {
+            return _fibonacciSequence[position - 1];
+        }
+
+        private List<int> Generate(int numberOfItems)
+        {
+            List<int> items = new List<int>(numberOfItems);
+            items.Add(_mostPreviousNumber);
+            items.Add(_immediatelyPreviousNumber);
+            items.Add(_currentNumber);
+            _mostPreviousNumber = 1;
+            _immediatelyPreviousNumber = 1;
+
+            for (int i = 0; i < numberOfItems; i++)
+            {
+                _currentNumber = _immediatelyPreviousNumber + _mostPreviousNumber;
+
+                items.Add(_currentNumber);
+
+                _mostPreviousNumber = _immediatelyPreviousNumber;
+                _immediatelyPreviousNumber = _currentNumber;
+            }
+
+            return items.Take(numberOfItems).ToList();
+        }
+    }
+}
---
+++ FibonacciSequenceTests/Sequencer.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+
+namespace FibonacciSequenceTests
+{
+    [TestFixture]
+    public class Sequencer
+    {
+        [Test]
+        public void GetPositionOfOneShouldReturnZero()
+        {
+            var fibonacciSequencer = new FibonacciSequence.Sequencer();
+            const int requestedPosition = 1;
+            const int expectedResult = 0;
+
+            var actualResult = fibonacciSequencer.GetPosition(requestedPosition);
+
+            Assert.That(expectedResult.Equals(actualResult));
+        }
+
+        [Test]
+        public void GetPositionOfTwoShouldReturnOne()
+        {
+            var fibonacciSequencer = new FibonacciSequence.Sequencer();
+            const int requestedPosition = 2;
+            const int expectedResult = 1;
+
+            var actualResult = fibonacciSequencer.GetPosition(requestedPosition);
+
+            Assert.That(expectedResult.Equals(actualResult));
+        }
+
+        [Test]
+        public void GetPositionOfThreeShouldReturnOne()
+        {
+            var fibonacciSequencer = new FibonacciSequence.Sequencer();
+            const int requestedPosition = 3;
+            const int expectedResult = 1;
+
+            var actualResult = fibonacciSequencer.GetPosition(requestedPosition);
+
+            Assert.That(expectedResult.Equals(actualResult));
+        }
+
+        [Test]
+        public void GetPositionOfFourShouldReturnTwo()
+        {
+            var fibonacciSequencer = new FibonacciSequence.Sequencer();
+            const int requestedPosition = 4;
+            const int expectedResult = 2;
+
+            var actualResult = fibonacciSequencer.GetPosition(requestedPosition);
+
+            Assert.That(expectedResult.Equals(actualResult));
+        }
+
+        [Test]
+        public void GetPositionOfTenShouldReturnThirtyFour()
+        {
+            var fibonacciSequencer = new FibonacciSequence.Sequencer();
+            const int requestedPosition = 10;
+            const int expectedResult = 34;
+
+            var actualResult = fibonacciSequencer.GetPosition(requestedPosition);
+
+            Assert.That(expectedResult.Equals(actualResult));
+        }
+    }
+}
